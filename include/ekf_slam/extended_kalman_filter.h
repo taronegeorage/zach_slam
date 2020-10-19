69b72efd59d99c769c19f170959727e810192543
taronegeorage <1155118584@link.cuhk.edu.hk>
2020-10-20 00:15:09 +0800

    ekf_slam: implement ekf correction step

--- include/ekf_slam/extended_kalman_filter.h
+++ include/ekf_slam/extended_kalman_filter.h
@@ -9,7 +9,12 @@ class EKF {
 public:
     EKF(Eigen::MatrixXd mu, Eigen::MatrixXd sigma, std::vector<sensors> sens, std::vector<int> obser):mu_(mu),sigma_(sigma),sensdata_(sens),observed_(obser) { 
         dim = sigma.cols(); 
-        R << motionNoise, 0, 0, 0, motionNoise, 0, 0, 0, motionNoise/10;
+        R = Eigen::MatrixXd::Constant(dim,dim,0);
+        R(0, 0) = motionNoise;
+        R(1, 1) = motionNoise;
+        R(2, 2) = motionNoise / 10;
+        Fxj = Eigen::MatrixXd::Constant(5,dim,0);
+        Fxj(0, 0) = 1; Fxj(1, 1) = 1; Fxj(2, 2) = 1;
     } 
     void param_init(Eigen::MatrixXd mu, Eigen::MatrixXd sigma, std::vector<sensors> sens, std::vector<int> obser);
     void prediction_step(int step);
@@ -22,7 +27,8 @@ private:
     std::vector<int> observed_;
     int dim;
     double motionNoise = 0.1;
-    Eigen::Matrix3d R;
+    Eigen::MatrixXd R;
+    Eigen::MatrixXd Fxj;
 };                                
                                   
 #endif
--- src/ekf_slam/extended_kalman_filter.cpp
+++ src/ekf_slam/extended_kalman_filter.cpp
@@ -22,8 +22,9 @@ void EKF::prediction_step(int step) {
     Gll.setIdentity();
     Eigen::MatrixXd G(dim, dim);
     G << Gx, Gl, Gl.transpose(), Gll;
+    Eigen::MatrixXd Te = sigma_ * G.transpose();
     cerr << "----" << endl;
-    cerr << G.matrix() << endl;
+    cerr << Te.matrix() << endl;
 
     // Compute the predicted sigma after incorporating the motion
     sigma_ = G * sigma_ * G.transpose() + R;
@@ -48,12 +49,43 @@ void EKF::correction_step(int step) {
         int ldmkId = laser[i][0];
         if(!observed_[ldmkId]) {
             // TODO: Initialize its pose in mu based on the measurement and the current robot pose:    
+            mu_(2*ldmkId+1,0) = mu_(0, 0) + laser[i][1] * cos(laser[i][2]+mu_(2,0));
+            mu_(2*ldmkId+2,0) = mu_(1, 0) + laser[i][1] * sin(laser[i][2]+mu_(2,0));
 
             observed_[ldmkId] = 1;
         }
         // TODO: Add the landmark measurement to the Z vector
+        Z(2*i, 0) = laser[i][1];
+        Z(2*i+1, 0) = laser[i][2];
 
+        // TODO: Use the current estimate of the landmark pose to compute the corresponding expected measurement in expectedZ:
+        double deltax = mu_(2*ldmkId+1, 0) - mu_(0, 0);
+        double deltay = mu_(2*ldmkId+2, 0) - mu_(1, 0);
+        double distance2 = deltax*deltax+deltay*deltay;
+        
+        expectedZ(2*i-2, 0) = sqrt(distance2);
+        expectedZ(2*i-1, 0) = remainder(atan2(deltay, deltax)-mu_(2, 0), 2*M_PI);
+
+        // TODO: Compute the Jacobian Hi of the measurement function h for this observation
+        Eigen::MatrixXd Hi(2, 5);
+        Hi << -sqrt(distance2)*deltax, -sqrt(distance2)*deltay, 0, sqrt(distance2)*deltax, sqrt(distance2)*deltay, 
+            deltay, -deltax, -distance2, -deltay, deltax;
+        
+        // Map Jacobian Hi to high dimensional space by a mapping matrix Fxj
+        Fxj(4, 2*ldmkId+1) = 1;
+        Fxj(5, 2*ldmkId+2) = 1;
+        Hi = Hi * Fxj;
+        //H.block<2, dim>(2*i, 0) = Hi;     
+        H.row(2*i) = Hi.row(0);
+        H.row(2*i+1) = Hi.row(1);     
     }
+    // TODO: Construct the sensor noise matrix Q
+
+    // TODO: Compute the Kalman gain
+
+    // TODO: Compute the difference between the expected and recorded measurements
+    
+    // TODO: Finish the correction step by computing the new mu and sigma.
 }
 
 Eigen::MatrixXd EKF::getmu() {
